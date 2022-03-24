e169e4b4057f48813d572075a3b9d2cd24259255
Kiu Man Yeung <118100055@umail.ucc.ie>
2022-03-24 13:51:00 +0000

    Scrolltext, line highlight, see, freeze unfreeze

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import ast
 import tkinter as tk
+import tkinter.scrolledtext as scrolledtext
 import shutil
 import os
 import traceback
@@ -11,14 +12,15 @@ class Application(tk.Frame):
     def __init__(self, master=None, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.winfo_toplevel().title("Python visualizer")
-        self.winfo_toplevel().bind('<Control-Left>', self.previous_img)
-        self.winfo_toplevel().bind('<Control-Right>', self.next_img)
-
+        self.winfo_toplevel().bind('<Control-Left>', self.previous)
+        self.winfo_toplevel().bind('<Control-Right>', self.next)
 
         self.img_path = os.path.join(os.path.curdir, "png")
-        self.index = 0
+        self.index_img = -1
+        self.index_line = -1
         self.graphs = []
         self.namespace = {}
+        self.execution_sequence = []
         self.status_variable = tk.StringVar()
         self.status_variable.set("Status: Ready")
         self.setup_widgets()
@@ -26,9 +28,13 @@ class Application(tk.Frame):
     def run(self):
         self.status_variable.set("Status: Executing")
         self.lbl_status.update()
+        self.lbl_right.configure(image='')
+        self.freeze()
 
-        self.index = 0
+        self.index_img = -1
+        self.index_line = -1
         self.namespace = {}
+        self.execution_sequence = []
         if os.path.isdir(self.img_path):
             shutil.rmtree(self.img_path)
         os.mkdir(self.img_path)
@@ -43,35 +49,61 @@ class Application(tk.Frame):
             try:
                 exec(compile(src.processed, filename="<src>", mode="exec"), self.namespace)
                 self.graphs = [f for f in os.listdir(self.img_path) if f.endswith(".png")]
-                if self.graphs:
-                    filename = os.path.join(self.img_path, self.graphs[0])
-                    img = tk.PhotoImage(file=filename)
-                    self.lbl_right.configure(image=img)
-                    self.lbl_right.image=img
-                else:
-                    self.lbl_right.configure(image='')
+                self.execution_sequence = self.namespace['execution_sequence']
+                print(self.execution_sequence)
+                self.next()
             except Exception as e:
                 self.lbl_right.configure(image='')
                 self.lbl_right.configure(text=traceback.format_exc())
 
     
+        self.status_variable.set("Status: Complete")
+
+    def next(self, event=None):
+        if self.index_line < len(self.execution_sequence)-1:
+            self.index_line += 1
+            line = float(self.execution_sequence[self.index_line][0])
+            self.highlight_line(line)
+            self.txt_edit.see(line)
+            if self.execution_sequence[self.index_line][1] == 1:
+                if self.index_img < len(self.graphs)-1:
+                    self.index_img += 1
+                    filename = os.path.join(self.img_path, self.graphs[self.index_img])
+                    img = tk.PhotoImage(file=filename)
+                    self.lbl_right.configure(image=img)
+                    self.lbl_right.image=img
+
+    def previous(self, event=None):
+        if self.index_line > 0:
+            self.index_line -= 1
+            line = float(self.execution_sequence[self.index_line][0])
+            self.highlight_line(line)
+            self.txt_edit.see(line)
+            if self.execution_sequence[self.index_line][1] == 1:
+                if self.index_img > 0:
+                    self.index_img -= 1
+                    filename = os.path.join(self.img_path, self.graphs[self.index_img])
+                    img = tk.PhotoImage(file=filename)
+                    self.lbl_right.configure(image=img)
+                    self.lbl_right.image=img
+    def edit(self):
+        self.unfreeze()
+        self.remove_highlight()
         self.status_variable.set("Status: Ready")
 
-    def next_img(self, event=None):
-        if self.index < len(self.graphs)-1:
-            self.index += 1
-            filename = os.path.join(self.img_path, self.graphs[self.index])
-            img = tk.PhotoImage(file=filename)
-            self.lbl_right.configure(image=img)
-            self.lbl_right.image=img
+    def freeze(self):
+        self.txt_edit.configure(state="disable")
 
-    def previous_img(self, event=None):
-        if self.index > 0:
-            self.index -= 1
-            filename = os.path.join(self.img_path, self.graphs[self.index])
-            img = tk.PhotoImage(file=filename)
-            self.lbl_right.configure(image=img)
-            self.lbl_right.image=img
+    def unfreeze(self):
+        self.txt_edit.configure(state="normal")
+
+    def highlight_line(self, line):
+        self.txt_edit.tag_remove("current_line", 1.0, "end")
+        lineno = str(line)
+        self.txt_edit.tag_add("current_line", lineno+" linestart", lineno+" lineend+1c")
+
+    def remove_highlight(self):
+        self.txt_edit.tag_remove("current_line", 1.0, "end")
 
     def setup_widgets(self):
         self.pack(fill="both", expand=True)
@@ -81,7 +113,6 @@ class Application(tk.Frame):
         self.rowconfigure(1, weight=0, minsize=20)
 
         self.lbl_status = tk.Label(self, textvariable=self.status_variable)
-        # self.lbl_status = tk.Label(self, text="Status: Ready")
         self.lbl_status.grid(row=1, column=0, columnspan=2, sticky="w")
 
         self.frm_left = tk.Frame(master=self, relief=tk.RAISED, borderwidth=1)
@@ -90,7 +121,8 @@ class Application(tk.Frame):
         self.frm_right = tk.Frame(master=self, relief=tk.RAISED, borderwidth=1)
         self.frm_right.grid(row=0,column=1, sticky="nsew")
 
-        self.frm_left.columnconfigure(0, weight=1, minsize=500)
+        self.frm_left.columnconfigure(0, weight=1, minsize=250)
+        self.frm_left.columnconfigure(1, weight=1, minsize=250)
         self.frm_left.rowconfigure(0, weight=1, minsize=470)
         self.frm_left.rowconfigure(1, weight=0, minsize=30)
 
@@ -99,13 +131,17 @@ class Application(tk.Frame):
         self.frm_right.rowconfigure(0, weight=1, minsize=470)
         self.frm_right.rowconfigure(1, weight=0, minsize=30)
 
-        self.txt_edit = tk.Text(master=self.frm_left, width=1, height=1)
-        self.txt_edit.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        self.txt_edit = scrolledtext.ScrolledText(master=self.frm_left, width=1, height=1)
+        self.txt_edit.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        self.txt_edit.tag_configure("current_line", background="#e9e9e9")
 
+        self.btn_edit = tk.Button(master=self.frm_left, text="Edit", height=2, command=self.edit)
+        self.btn_edit.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
         self.btn_run = tk.Button(master=self.frm_left, text="Run", height=2, command=self.run)
-        self.btn_previous = tk.Button(master=self.frm_right, text="Previous", height=2, command=self.previous_img)
-        self.btn_next = tk.Button(master=self.frm_right, text="Next", height=2, command=self.next_img)
-        self.btn_run.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        self.btn_run.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
+
+        self.btn_previous = tk.Button(master=self.frm_right, text="Previous", height=2, command=self.previous)
+        self.btn_next = tk.Button(master=self.frm_right, text="Next", height=2, command=self.next)
         self.btn_previous.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
         self.btn_next.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
 
@@ -138,7 +174,7 @@ import os
         
         self.globals = ast.parse('''
 global png_counter
-global graph_lines''')
+global execution_sequence''')
 
         self.graphs = insert = ast.parse('''
 mySpecialGraph = callsviz(varnames=[''' + "'" + self.vars + "'" + '''])
@@ -156,8 +192,8 @@ mySpecialGraph.render(os.path.join(os.path.curdir, "png", str(png_counter).zfill
         self.insert_global(self.processed)
         self.insert_imports(self.processed)
 
-    def lineno_builder(self, line_numbers):
-        insert = ast.parse('''print("Executing line: '''+ str(line_numbers) +'''")''')
+    def lineno_builder(self, line_numbers, graph):
+        insert = ast.parse('''execution_sequence.append(('''+ str(line_numbers) +''','''+ str(graph) +'''))''')
         return insert
         
     def clean_lineno(self, node, lineno):
@@ -178,6 +214,9 @@ mySpecialGraph.render(os.path.join(os.path.curdir, "png", str(png_counter).zfill
                     if not isinstance(element, self.do_not_follow):
                         self.clean_lineno(self.graphs, element.lineno)
                         temp = temp + self.graphs.body
+                        temp = temp + self.lineno_builder(element.lineno, 1).body
+                    else:
+                        temp = temp + self.lineno_builder(element.lineno, 0).body
                 node.orelse = temp
 
                 for element in node.finalbody:
@@ -188,6 +227,9 @@ mySpecialGraph.render(os.path.join(os.path.curdir, "png", str(png_counter).zfill
                     if not isinstance(element, self.do_not_follow):
                         self.clean_lineno(self.graphs, element.lineno)
                         temp = temp + self.graphs.body
+                        temp = temp + self.lineno_builder(element.lineno, 1).body
+                    else:
+                        temp = temp + self.lineno_builder(element.lineno, 0).body
                 node.finalbody = temp
 
             for element in node.body:
@@ -196,10 +238,14 @@ mySpecialGraph.render(os.path.join(os.path.curdir, "png", str(png_counter).zfill
             if isinstance(node, self.do_global) :
                 temp = temp + self.globals.body
             for element in node.body:
-                temp.append(element)
                 if not isinstance(element, self.do_not_follow):
+                    temp = temp + self.lineno_builder(element.lineno, 1).body
+                    temp.append(element)
                     self.clean_lineno(self.graphs, element.lineno)
                     temp = temp + self.graphs.body
+                else:
+                    temp = temp + self.lineno_builder(element.lineno, 0).body
+                    temp.append(element)
             # if isinstance(node, ast.Module):
             #     temp = temp[:-4]
             node.body = temp
@@ -210,8 +256,8 @@ mySpecialGraph.render(os.path.join(os.path.curdir, "png", str(png_counter).zfill
     def insert_global(self, node):
         global_variables = ast.parse('''global png_counter
 png_counter = 0
-global graph_lines
-graph_lines = []''')
+global execution_sequence
+execution_sequence = []''')
         i = 0
         for i in range(len(node.body)):
             if not isinstance(node.body[i], (ast.Import, ast.ImportFrom)):
